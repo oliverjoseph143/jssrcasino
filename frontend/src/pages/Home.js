5bcae46c6d5f423e7ef1b153eccfc715cdd620ba
oliverjoseph143 <jssrmanagement@jssrmanagement.com>
2025-09-05 22:13:19 +0530

    to commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -626,8 +626,8 @@ const Home = () => {
         }
         
         .mobile-nav-img {
-          width: 40px;
-          height: 40px;
+          width: 30px;
+          height: 30px;
         }
         
         .mobile-nav-text {
